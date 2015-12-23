fbebc98991b6fa950dd6a37170b4634b47058221
Brian Ethier <davidbrianethier@gmail.com>
2015-12-22 19:54:15 -0500

    Have a login form working on the homepage. Still in the process of debugging the username upsert

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,31 @@
 var app = angular.module('pandaJuice', ['lbServices']);
 
-app.controller('rootController', ['$scope', function($scope) {
+app.controller('rootController', ['$scope', 'User', function($scope, User) {
+  console.log("Hitting controller method");
   $scope.oreo = "Oreo Bro";
+  $scope.email = "admin@pandaJuice.com";
+  $scope.password = "fossil";
+
+  $scope.login = function(email, password) {
+    console.log("Logging in with email: %s and password: %s", email, password);
+    User.login({email: email, password: password}).$promise.then(
+      function(ret) {
+        console.log("Successfully logged in user: ", ret.user);
+        $scope.user = ret.user;
+      }, 
+      function(err) {
+        console.log("Failed to login user. Reason: ", err);
+      });
+  }
+
+  $scope.setUsername = function(username) {
+    console.log("Changing username to: ", username);
+    User.upsert({id: $scope.user.id, username: username}, function(a,b,c) {
+      console.log("callback a: ", a);
+      console.log("callback b: ", b);
+      console.log("callback c: ", c);
+    });
+  }
 }]);
 
 
