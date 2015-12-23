6216e2b3546781d4aefabd7c55a40e9229badebc
Brian Ethier <davidbrianethier@gmail.com>
2015-12-22 20:28:57 -0500

    Changed ACLs and a few view things. Going to overrride user soon

--- client/js/app.js
+++ client/js/app.js
@@ -1,26 +1,51 @@
 var app = angular.module('pandaJuice', ['lbServices']);
 
-app.controller('rootController', ['$scope', 'User', function($scope, User) {
+app.controller('rootController', ['$scope', 'User', 'Item', function($scope, User, Item) {
   console.log("Hitting controller method");
-  $scope.oreo = "Oreo Bro";
+
+  //TODO: Take this out, just for ease of testing
   $scope.email = "admin@pandaJuice.com";
   $scope.password = "fossil";
 
   $scope.login = function(email, password) {
     console.log("Logging in with email: %s and password: %s", email, password);
-    User.login({email: email, password: password}).$promise.then(
+    var loginResult = User.login({email: email, password: password},
       function(ret) {
+        console.log("In the success function");
+        console.log("Ret: ", ret);
         console.log("Successfully logged in user: ", ret.user);
-        $scope.user = ret.user;
+
+        /*
+        User.findById(ret.user.id, function(err, o) {
+          console.log("Found user by userId of logged in user");
+          console.log(o);
+          $scope.user = o;
+        });
+        */
+
+        console.log("Logged in userid: ", User.getCurrentId());
+        var oreoUser = User.findById(1);
+        console.log("oreoUser: ", oreoUser);
+
+        var oreoItem = Item.findById({id: 1});
+        console.log("oreoItem: ", oreoItem);
+
+        window.oreoUser = oreoUser;
+        window.oreoItem = oreoItem;
       }, 
       function(err) {
         console.log("Failed to login user. Reason: ", err);
       });
+
+    window.loginres = loginResult;
   }
 
   $scope.setUsername = function(username) {
     console.log("Changing username to: ", username);
-    User.upsert({id: $scope.user.id, username: username}, function(a,b,c) {
+
+    $scope.user.username = username;
+
+    User.upsert($scope.user, function(a,b,c) {
       console.log("callback a: ", a);
       console.log("callback b: ", b);
       console.log("callback c: ", c);
